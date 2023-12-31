86be936ddbbfde1d611686e02bca0470ee2baf26
KasunUmesh <kasunumesh38@gmail.com>
2024-01-01 02:00:14 +0530

    apply express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,9 @@
-console.log("Hello!");
-console.log("Hello Typescript")
+import express from 'express';
+
+// invoke the express
+const app = express();
+
+// Start the server
+app.listen(8081, () => {
+    console.log("Server started on port 8080");
+})
