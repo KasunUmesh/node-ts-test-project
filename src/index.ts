eb05abd77a23906bd8edf25239a445df5dc9952f
KasunUmesh <kasunumesh38@gmail.com>
2024-01-02 01:51:58 +0530

    dev first route

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,19 @@ import express from 'express';
 // invoke the express
 const app = express();
 
+app.get('/user/all', (req: express.Request, res: express.Response) => {
+
+    let data = {
+        _id: "U001",
+        username: "Pathum",
+        fname: "Pathum",
+        lname: "silva",
+        email: "pathum@example.com"
+    }
+
+    res.send(data);
+})
+
 // Start the server
 app.listen(8081, () => {
     console.log("Server started on port 8080");
