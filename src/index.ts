bb037518b5552102edd08a648209d81dcdde5f4a
KasunUmesh <kasunumesh38@gmail.com>
2024-01-02 05:37:56 +0530

    update db connection

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from "body-parser";
-import * as mongoose from "mongoose";
+import mongoose from "mongoose";
 
 // invoke the express
 const app = express();
