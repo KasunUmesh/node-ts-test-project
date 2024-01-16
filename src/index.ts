4edca7678e6a6023b1eeea18fd0df55cce08cf2a
KasunUmesh <kasunumesh38@gmail.com>
2024-01-17 03:23:40 +0530

    dev delete an articles

--- src/index.ts
+++ src/index.ts
@@ -317,6 +317,37 @@ app.put('/articles', verifyToken, async (req: express.Request, res: any) => {
     }
 })
 
+app.delete('/articles/:id', verifyToken, async (req: express.Request, res: any)=> {
+    try {
+        let user_id = res.tokenData.user._id;
+
+        let article_id: string = req.params.id;
+
+        let article = await ArticleModel.find({_id: article_id, user: user_id})
+
+        if (article) {
+
+            await ArticleModel.deleteOne({_id: article_id}).then(r => {
+                res.status(200).send(
+                    new CustomResponse(200, "Article deleted successfully")
+                )
+            }).catch(e => {
+                res.status(100).send(
+                    new CustomResponse(100, "Something went wrong")
+                )
+            })
+
+        } else {
+            res.status(401).send(
+                new CustomResponse(401, "Access denied")
+            )
+        }
+
+    } catch (error) {
+        res.status(100).send("Error")
+    }
+})
+
 // Start the server
 app.listen(8081, () => {
     console.log("Server started on port 8080");
