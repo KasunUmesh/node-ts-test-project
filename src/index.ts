182f2424d9516f65918ba861af3442a18e96c52e
KasunUmesh <kasunumesh38@gmail.com>
2024-01-17 03:04:29 +0530

    dev update articles

--- src/index.ts
+++ src/index.ts
@@ -282,6 +282,41 @@ app.get('/articles/get/my', verifyToken, async (req: express.Request, res: any) 
     }
 })
 
+app.put('/articles', verifyToken, async (req: express.Request, res: any) => {
+    try {
+
+        let req_body: any = req.body
+
+        let user_id = res.tokenData.user._id;
+
+        let article = await ArticleModel.find({_id: req_body.id, user: user_id})
+
+        if (article) {
+
+            await ArticleModel.findOneAndUpdate({_id: req_body.id}, {
+                title: req_body.title,
+                description: req_body.description
+            }).then(r => {
+                res.status(200).send(
+                    new CustomResponse(200, "Article updated successfully")
+                )
+            }).catch(e => {
+                res.status(100).send(
+                    new CustomResponse(100, "Something went wrong")
+                )
+            })
+
+        } else {
+            res.status(401).send(
+                new CustomResponse(401, "Access denied")
+            )
+        }
+
+    } catch (error) {
+        res.status(100).send("Error");
+    }
+})
+
 // Start the server
 app.listen(8081, () => {
     console.log("Server started on port 8080");
